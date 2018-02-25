9ee546ff298ec8c2df7801c4464f83e2db543ca6
Toshiba <Toshiba@LAPTOP-I59SQ56I>
2018-02-25 08:03:01 -0600

    Tarea 3 Finalizada

---
+++ src/menus/Menu.java
@@ -0,0 +1,17 @@
+
+package menus;
+
+import java.util.Scanner;
+
+
+public class Menu {
+
+    public static void main(String[] args) {
+  
+    Menu_Princ menu= new Menu_Princ();       
+}
+        
+    
+
+    
+}
